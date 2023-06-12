68c255c5018068c5d8c5460f2a692d5a1781e6a1
Abolfazl Norzad <abolfazlnorzad@yahoo.com>
2023-06-12 12:00:56 +0330

    example for proxy pattern :  Cached Repository

---
+++ src/Proxy/CachedRepository/CacheProductRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Src\Proxy\CachedRepository;
+
+use Illuminate\Cache\Repository;
+
+class CacheProductRepository implements ProductRepositoryInterface
+{
+    private ProductRepositoryInterface $productRepository;
+    private Repository $cache;
+
+    /**
+     * @param ProductRepositoryInterface $productRepository
+     * @param Repository $cache
+     */
+    public function __construct(ProductRepositoryInterface $productRepository, Repository $cache)
+    {
+        $this->productRepository = $productRepository;
+        $this->cache = $cache;
+    }
+
+
+    public function getAll()
+    {
+        $result = $this->cache->get("all-product");
+        if (is_null($result)){
+            $this->cache->set("all-product",$this->productRepository->getAll());
+        }
+        return $result;
+    }
+}
---
+++ src/Proxy/CachedRepository/EloquentProductRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Src\Proxy\CachedRepository;
+
+class EloquentProductRepository implements ProductRepositoryInterface
+{
+
+    public function getAll()
+    {
+        // TODO: Implement getAll() method.
+    }
+}
